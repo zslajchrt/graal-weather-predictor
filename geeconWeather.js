fc151c459011fe3b49c4983967d677a683bbe054
Zbynek Slajchrt <zbynek.slajchrt@oracle.com>
2016-10-24 13:58:27 +0200

    Comments added to scripts

--- geeconWeather.js
+++ geeconWeather.js
@@ -1,16 +1,20 @@
 fs = require('fs');
 
+// Load the Ruby module
 console.log("Initializing Openweather");
 var weatherInitScript = fs.readFileSync("geeconWeatherInit.rb", "utf8");
 Interop.eval("application/x-ruby", weatherInitScript);
 
+// Load the R module
 console.log("Preparing weather model");
 var weatherModelScript = fs.readFileSync("geeconWeatherModel.r", "utf8");
 Interop.eval("application/x-r", weatherModelScript);
 
+// Import the function exported from the R module
 predictTemp = Interop.import('predictTemp');
 realTemp = Interop.import('realTemp');
 
+// A helper to convert the temperatures to a JSON object
 function jsonify(cityName, realTmp, predictedTmp) {
 	return JSON.stringify({
 		city: cityName, 
@@ -19,6 +23,7 @@ function jsonify(cityName, realTmp, predictedTmp) {
 	});
 }
 
+// Launch the server
 console.log("Starting server...");
 var http = require("http");
 var server = http.createServer(function (inp, out) {
--- geeconWeatherInit.rb
+++ geeconWeatherInit.rb
@@ -1,13 +1,16 @@
 require 'openweather2'
 
+# Initialize the OpenWeather service
 Openweather2.configure do |config| 
 	config.endpoint = 'http://api.openweathermap.org/data/2.5/weather'
 	config.apikey = 'dd7073d18e3085d0300b6678615d904d'
 end
 
+# Return the current temperature in the given city
 def tempInCity(name) 
 	weather = Openweather2.get_weather(city: Truffle::Interop.from_java_string(name), units: 'metric')
 	weather.temperature; 
 end
 
+# Export the tempInCity function so that it can be used in other languages
 Truffle::Interop.export('tempInCity', method(:tempInCity))
